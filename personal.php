1a48d34bf44acaa95c3f8324dcaf913d25658193
albesquivel <alberto.esquivel@sjsu.edu>
2022-10-24 23:41:24 -0700

    Create personal.php

---
+++ personal.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Processing File</title>
+  </head>
+  <body>
+    <h1>Processing</h1>
+    <?php
+      if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email"])) {
+        if  ($_POST["username"] && $_POST["password"] && $_POST["email"]){
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $email = $_POST["email"]
+
+        //create connection
+        $conn = mysqli_connect("localhost", "root", "", "robank");
+
+        //check connection
+        if (!$conn){
+          die("Connection failed: " . mysqli_connect_error());
+        }
+
+        //register user
+        $sql = "INSERT INTO client (username, password, email) VALUES ('$username', '$password', '$email')";
+
+        $results = mysqli_query($conn, $sql);
+
+        if ($results) {
+          echo "The user has been added.";
+        } else {
+          echo mysqli_error($conn);
+        }
+
+        mysqli_close($conn); //close connection
+
+        } else {
+          echo "Username or password is empty.";
+        }
+
+      } else {
+        echo "Form was not submitted.";
+      }
+    ?>
+  </body>
+</html>
