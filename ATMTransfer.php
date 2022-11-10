042d2c5218ea973d4684821802debaefef31e449
Josh <joshua.medina@sjsu.edu>
2022-11-09 17:53:54 -0800

    Updated the Transfer page.

--- ATMTransfer.php
+++ ATMTransfer.php
@@ -1,3 +1,65 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
+    header('Location: /robank/ATMLogin.php');
+  }
+  
+  
+  $conn = mysqli_connect("localhost", "root", "", "bank");
+  
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  
+  function list_from_accounts() {
+  global $conn;
+
+  $id = $_POST['account_id'];
+  $sql = "SELECT accountname FROM accounts where username LIKE (SELECT username FROM accounts WHERE account = '$id');";
+  $results = mysqli_query($conn, $sql);
+  if($results)
+  {
+    while($row = mysqli_fetch_assoc($results)){
+      // echo "<a href='/robank/ATMOptions.php?id=" . $row['account'] ."'>" . $row['accountname'] . "</a>";
+      ?>
+        <div class = "one">
+          <form action="#" method="post">
+            <input type="hidden" name="account_id" value="<?= $_POST['account_id']  ?>">
+            <button class = "#"type="submit"><?= $row['accountname'] ?></button>
+          </form>
+        </div>
+      <?php
+        
+        }
+    }
+}
+
+    function list_to_accounts() {
+        global $conn;
+      
+        $id = $_POST['account_id'];
+        $sql = "SELECT accountname FROM accounts where username LIKE (SELECT username FROM accounts WHERE account = '$id');";
+        $results = mysqli_query($conn, $sql);
+        if($results)
+        {
+          while($row = mysqli_fetch_assoc($results)){
+            // echo "<a href='/robank/ATMOptions.php?id=" . $row['account'] ."'>" . $row['accountname'] . "</a>";
+            ?>
+              <div class = "one">
+                <form action="#" method="post">
+                  <input type="hidden" name="account_id" value="<?= $_POST['account_id']  ?>">
+                  <button class = "#"type="submit"><?= $row['accountname'] ?></button>
+                </form>
+              </div>
+            <?php
+              
+              }
+          }
+}
+
+?>
+
 <html>
   <head>
     <meta charset="UTF-8">
@@ -25,11 +87,33 @@
 
     <div class = "bottomBox">
     
-        <div class ="depoInfo">
-        <?= $_POST['account_id'] ?>
+    <div class ="depoInfo">
+    <!-- <?= $_POST['account_id'] ?> -->
+        <div class = "topboxbalance">
+            <div class = "topboxbalanceleft">From Account:</div>
+            <div class = "topboxbalanceright"> <?= list_from_accounts() ?> </div>
         </div>
 
-    </div>
+        <div class = "topboxbalance">
+            <div class = "topboxbalanceleft">To Account:</div>
+            <div class = "topboxbalanceright"> <?= list_to_accounts() ?> </div>
+        </div>
 
+        <div class = "topboxbalance">
+            <div class = "topboxbalanceleft">Enter Transfer Amount</div>
+        </div>
+
+        <div class = "topboxbalance">
+            <form action="#" method="post">
+            <label for="amountentered"></label>
+            <input type="text" id="amountentered"  name="amountentered"><br><br>
+            <div class="submitBtnone">
+            <input type="submit" value="Submit">
+            </div>
+            </form>
+        </div>
+
+        </div>
+    </div>
   </body>
 </html>
