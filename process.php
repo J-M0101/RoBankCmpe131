d6a720d2113c42ab58b677dede7b7318f07865e4
Josh <joshua.medina@sjsu.edu>
2022-11-14 16:02:45 -0800

    updated Createuser and process page

--- process.php
+++ process.php
@@ -1,77 +1,85 @@
 <html>
-<!--Header and Title-->
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="styles.css">
-  <title>Robobank Processing</title>
-</head>
-<body>
-<!-- Robobank Top of bar box. Designed with CSS flexdispalays.  -->
-                  <div class = "topBox">
-                    <div class = "leftBoxL">
-                      <button class="toplink"><a href="ATMLogin.html" id="topcolor">RoBank</a></button>
-                    </div>
-                    <div class = "buttonGroup">
-                      <div class = "rightBoxL">
-                        <button class="toplink"><a href="#" id="topcolor">Home</a></button>
-                      </div>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Styles.css">
+    <title>Account Info</title>
+  </head>
 
-                      <div class = "rightBoxR">
-                        <button class="toplink"><a href="accountInfo.html" id="topcolor">Login</a></button>
-                      </div>
-                    </div>
-                  </div>
+  <body>
+    
+    <div class = "topBox">
+
+      <div class = "leftBoxL">
+        <button class="toplink"><a href="/robank/accountLogin.html" id="topcolor">RoBank</a></button>
+      </div>
+      <div class = "buttonGroup">
+        <div class = "rightBoxL">
+          <button class="toplink"><a href="/robank/accountInfo.php" id="topcolor">How To Use</a></button>
+        </div>
+
+        <div class = "rightBoxR">
+          <button class="toplink"><a href="/robank/ATMLogin.php" id="topcolor">Login</a></button>
+        </div>
+      </div>
+    </div>
 
 <!-- Robobank Page Title (left)  -->
-                  <div class = "bottomBox">
-                    <div class = "centerBox">
-                        <div class = "centerLeftBox">
-                          <p class="atmLogin">Account Processing</p>
-                        </div>
-                    </div>
-                  </div>
-<!--processing forms-->
-            <div class="section row">
-                <div class="section-text col-right">
-                    <?php
-                      if (isset($_POST["username"]) && isset($_POST["password"])){
-                        if (($_POST["username"]) && ($_POST["password"])){
-                          $username = $_POST["username"];
-                          $password = $_POST["password"];
-                          //echo "Username from registration: " . $_POST["username"];
-                          //echo "<BR>";
-                          //echo "Password from registration: " . $_POST["password"];
+    <div class = "bottomBox">
+      <div class = "centerBox">
+          <div class = "centerLeftBox">
+            <p class="atmLogin">Account Processing</p>
+          </div>
+      </div>
+    </div>
 
-                          // create connection
-                          $conn = mysqli_connect("localhost", "root", "", "users");
+    <!--processing forms-->
+    <div class="section row">
+      <div class="section-text col-right">
+        <?php
+          if (isset($_POST["username"]) && isset($_POST["password"])){
+            if (($_POST["username"]) && ($_POST["password"])){
+              $username = $_POST["username"];
+              $password = $_POST["password"];
+              $firstname = $_POST["firstname"];
+              $lastname = $_POST["lastname"];
+              $phone = $_POST["phone"];
+              $email = $_POST["email"];
+              $address = $_POST["address"];
 
-                          // check connection
-                          if (!$conn){
-                            die("Connetion failed: " . mysqli_connect_error());
-                          }
+              //echo "Username from registration: " . $_POST["username"];
+              //echo "<BR>";
+              //echo "Password from registration: " . $_POST["password"];
 
-                          // register user
-                          $sql = "INSERT INTO students (username, password) VALUES ('$username', '$password')";
+              // create connection
+              $conn = mysqli_connect("localhost", "root", "", "bank");
 
-                          $results = mysqli_query ($conn, $sql);
+              // check connection
+              if (!$conn){
+                die("Connetion failed: " . mysqli_connect_error());
+              }
 
-                          if ($results){
-                            echo "The user has been added.";
-                          } else {
-                            echo mysqli_error($conn);
-                          }
+              // register user
+              $sql = "INSERT INTO users (username, password, firstname, lastname, phone, email, address) VALUES ('$username', '$password', '$firstname', '$lastname', '$phone', '$email', '$address')";
 
-                          mysqli_close($conn); // close connection
+              $results = mysqli_query ($conn, $sql);
 
-                        } else {
-                          echo "Username or password is empty.";
-                        }
+              if ($results){
+                echo "The user has been added.";
+              } else {
+                echo mysqli_error($conn);
+              }
 
-                      } else {
-                        echo "Form was not submitted.";
-                      }
-                     ?>
-                </div>
-            </div>
-    </body>
+              mysqli_close($conn); // close connection
+
+            } else {
+              echo "Username or password is empty.";
+            }
+
+          } else {
+            echo "Form was not submitted.";
+          }
+        ?>
+      </div>
+    </div>
+  </body>
 </html>
