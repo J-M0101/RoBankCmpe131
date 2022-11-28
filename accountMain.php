9cd433014bec0b89507a065a6cbaee27a3aa0383
dmaighty <115969524+dmaighty@users.noreply.github.com>
2022-11-27 19:59:55 -0800

    removed <style>

--- accountMain.php
+++ accountMain.php
@@ -69,53 +69,6 @@
         </div>
         <div class = "buttonGroup">
 
-          <style>
-            .dropbtn {
-              display: flex;
-              background-color: #014421;
-              align-self: center;
-              border: none;
-              font-size: 25px;
-            }
-
-            .dropdown {
-              position: relative;
-              display: inline-block;
-              display: flex;
-              margin-right:5rem;
-            }
-
-            .dropdown-content {
-              display: none;
-              position: absolute;
-              background-color: #f1f1f1;
-              min-width: 160px;
-              box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-              z-index: 1;
-            }
-
-            .dropdown-content a {
-              color: black;
-              padding: 12px 16px;
-              text-decoration: none;
-              display: block;
-            }
-
-            .dropdown-content a:hover {background-color: #ddd;}
-
-            .dropdown:hover .dropdown-content {display: block;}
-
-            .dropdown:hover .dropbtn {background-color: #3e8e41;}
-            </style>
-
-            <div class="dropdown">
-              <button class="dropbtn" id="topcolor">Checking Account: xxxx-xxxx-xxxx-7890</button>
-              <div class="dropdown-content">
-                <a href="#">Checking Account: xxxx-xxxx-xxxx-1246</a>
-                <a href="#">Saving Account: xxxx-xxxx-xxxx-6343</a>
-                <a href="#">Saving Account: xxxx-xxxx-xxxx-6844</a>
-              </div>
-            </div>
 
             <div class = "rightBoxR">
               <button class="toplink"><a href="accountLogin.php" id="topcolor">LOGOUTTest</a></button>
