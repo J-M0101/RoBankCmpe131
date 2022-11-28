bbc4d48b57c3316a660dee636b38048727383803
Josh <joshua.medina@sjsu.edu>
2022-11-27 16:50:23 -0800

    Withdraw of working

--- ATMCashWithdaw.php
+++ ATMCashWithdaw.php
@@ -2,7 +2,7 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST["amountentered"])) 
+    if (isset($_POST["amountentered"]) && isset($_POST["account_id"])) 
     {   
         $useramount = $_POST["amountentered"];
         $accountnumber = $_POST["account_id"];
@@ -12,24 +12,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             die("Connection failed: " . mysqli_connect_error());
         }
 
-        function list_balance($accountnumber) {
-            global $conn;
-
-            $sql = "SELECT balance FROM accounts WHERE account = '$accountnumber';";
-            $results = mysqli_query($conn, $sql);
-            if($results){
-                $row = mysqli_fetch_assoc($results);
-                
-                while($row = mysqli_fetch_assoc($results)){
-                    $balance = $row['balance'];
-                }
-            }
-
+        $sql = "UPDATE accounts SET balance = balance - '$useramount' WHERE account = '$accountnumber';";
+        
+        $results = mysqli_query($conn, $sql);
+        if($results){
+            header("Location: accountInfo.php");
         }
-    }
-    else
-    {
-        $error_message = "Missing input";
+
     }
 }
 ?>
@@ -61,23 +50,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 
     <div class = "bottomBox">
-    
         <div class ="depoInfo">
-            <!-- <?= $_POST["account_id"]?> -->
             <div class = "topboxbalance">
-                <div class = "topboxbalanceleft">Enter Withdraw Amount</div>
+                <div class = "topboxbalanceleft">Enter Withdraw Amount
+                <?php
+                if (isset($error_message)) {
+                ?>
+                <p><?= $error_message ?></p>
+                <?php
+                }
+                ?>
+            </div>
             </div>
             <div class = "topboxbalance">
-                <form action="Test.php" method="post">
-                <label for="amountentered"></label>
-                <input type="number" id="amountentered"  name="amountentered"><br><br>
-                <div class="submitBtnone">
-                <input type="submit" value="Submit">
+                <form action="" method="post">
+                    <input type="hidden" name="account_id" value="<?= $_POST["account_id"] ?>">
+                    <label for="amountentered"></label>
+                    <input type="number" id="amountentered" name="amountentered"><br><br>
+                    <div class="submitBtnone">
+                    <input type="submit" value="Submit">
+                    </div>
+                </form>
             </div>
-            </form>
         </div>
-        </div>
-
     </div>
 
   </body>
