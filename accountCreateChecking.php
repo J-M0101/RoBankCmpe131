803e98c62682e20a2f5566b78c24d1fd5a3beb8a
dmaighty <115969524+dmaighty@users.noreply.github.com>
2022-12-01 17:46:30 -0800

    final changes to create

--- accountCreateChecking.php
+++ accountCreateChecking.php
@@ -52,11 +52,7 @@
     }
     ?>
     <form action="accountCreateSavings.php" method="post">
-<<<<<<< HEAD
      <input type="number" name = "pin" max = "9999">
-=======
-     <input type="number" name = "pin">
->>>>>>> parent of 0f00245 (Bugfix Checking/Saving, redirect ATM login to ATMOptions.php)
               <!--<input type="submit" min="0" id="amount" name="amount" value = "Transfer funds">-->
      <input type="submit" name="transfer" value="Enter a pin number">
     </form>
